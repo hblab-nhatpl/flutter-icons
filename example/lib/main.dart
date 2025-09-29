776f90331774beb1f4e986d3964aff2ce37610d8
F10S Mobile Team <f10smobileteam@gmail.com>
2025-09-29 11:39:09 +0600

    Refactor: Improve code readability and debugging in example app
    This commit introduces several changes to the example app to improve readability and debugging capabilities:
    
    - Updated `MyApp.build` and `_MyHomePageState.build` methods to use arrow syntax for conciseness.
    - Reordered the `key` parameter in the `MyHomePage` constructor to follow convention.
    - Added `debugFillProperties` to `MyHomePage` for better debugging information.
    - Added `const` to `EdgeInsets.all` for improved performance.
    - Explicitly typed the `List` of widgets in `_MyHomePageState.build`.
    - Imported `flutter/foundation.dart` for `DiagnosticPropertiesBuilder`.
    
    Signed-off-by: F10S Mobile Team <f10smobileteam@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
@@ -8,70 +9,73 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Icons'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Icons'),
+      );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('title', title));
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // This method is rerun every time setState is called, for instance as done
+  // by the _incrementCounter method above.
+  //
+  // The Flutter framework has been optimized to make rerunning build methods
+  // fast, so that you can just rebuild anything that needs updating rather
+  // than having to individually change instances of widgets.
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(AntDesign.stepforward),
-            Icon(Ionicons.ios_search),
-            Icon(FontAwesome.glass),
-            Icon(MaterialIcons.ac_unit),
-            Icon(FontAwesome5.address_book),
-            Icon(FontAwesome5Solid.address_book),
-            Icon(FontAwesome5Brands.$500px),
-            Icon(FlutterIcons.$500px_ent),
-            Icon(FlutterIcons.$500px_faw5d),
-            Icon(WeatherIcons.wi_alien),
-            Icon(WeatherIcons.wi_cloud),
-            Icon(WeatherIcons.wi_day_rain)
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-    );
-  }
+        body: const Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(AntDesign.stepforward),
+              Icon(Ionicons.ios_search),
+              Icon(FontAwesome.glass),
+              Icon(MaterialIcons.ac_unit),
+              Icon(FontAwesome5.address_book),
+              Icon(FontAwesome5Solid.address_book),
+              Icon(FontAwesome5Brands.$500px),
+              Icon(FlutterIcons.$500px_ent),
+              Icon(FlutterIcons.$500px_faw5d),
+              Icon(WeatherIcons.wi_alien),
+              Icon(WeatherIcons.wi_cloud),
+              Icon(WeatherIcons.wi_day_rain),
+            ],
+          ),
+        ),
+      );
 }
